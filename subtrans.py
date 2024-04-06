bdb9b4a6a6011cd754d7d20ec2a7ca9c8a2bb3f0
sirlaurie <liusuzhang@gmail.com>
2024-04-07 01:01:41 +0800

    added support for progress handleing and file path management

--- subtrans.py
+++ subtrans.py
@@ -5,6 +5,7 @@
 import os
 import argparse
 import subprocess
+import math
 from getpass import getpass
 from typing import Union
 
@@ -29,7 +30,7 @@ else:
         else:
             print("无效的API Key. 请重试")
 
-openai_client = OpenAI(api_key=api_key)
+openai_client = OpenAI(api_key=api_key, timeout=180.0, max_retries=5)
 
 index_lang = {}
 
@@ -46,14 +47,22 @@ def pprint(sub_streams: str) -> None:
 
 
 class SubtitleGenerator(object):
-    def __init__(self, video: str, target_lang: str = "简体中文", max_split: int = 80):
+    def __init__(
+        self,
+        video: str,
+        target_lang: str = "简体中文",
+        max_split: int = 80,
+        output_dir: str | None = None,
+    ):
         self.video = video
         self.video_name = ".".join(video.split(".")[:-1])
         self.max_split = max_split
         self.target_lang = target_lang
         self.target_subtitle = SSAFile()
+        self.output_dir = output_dir
 
     def extract(self) -> Union[str, None]:
+        self.input_dir = os.path.dirname(os.path.abspath(self.video))
         output = subprocess.check_output(
             [
                 "ffprobe",
@@ -70,13 +79,17 @@ class SubtitleGenerator(object):
         )
         if not output:
             print("\n当前视频没有内置软字幕")
-            return
+            return None
 
         sub_streams = output.decode()
         pprint(sub_streams)
 
         index = input("选择字幕流: ")
-        target_sub_file = f"{self.video_name}.srt"
+        target_sub_file = os.path.join(self.input_dir, f"{self.video_name}.srt")
+        if self.output_dir:
+            target_sub_file = os.path.join(
+                os.path.abspath(self.output_dir), f"{self.video_name}.srt"
+            )
         print("抽取已选择的字幕流 ... ", end="")
 
         out_sub = subprocess.check_output(
@@ -94,13 +107,15 @@ class SubtitleGenerator(object):
         )
 
         if out_sub:
-            exit(f"抽取字幕时发生错误: {out_sub}")
+            print(f"抽取字幕时发生错误: {out_sub}")
+            return None
         print("OK!")
         return target_sub_file
 
     def load(self, subtitle_file: str) -> None:
         subtile = pysubs2.load(subtitle_file)
         self.source_subtitle = subtile
+        os.remove(subtitle_file)
 
     def split(self, subtitle: SSAFile, start: int) -> SSAFile:
         slices_subtitle = SSAFile()
@@ -123,7 +138,7 @@ class SubtitleGenerator(object):
             messages=[
                 {
                     "role": "system",
-                    "content": f"你是一个专业的电影字幕翻译人员, 擅长从各种语言翻译为中文. 用户会将带有时间轴信息的字幕发送给你, 你要先完整理解字幕内容, 然后保留字幕的序列号和时间轴信息, 将字幕翻译为{self.target_lang}. 要求: 1,保持输出内容格式和输入内容格式相同.2,保留字幕中的人名和专有名词和缩写.3,输出内容要符合中文使用习惯,也要符合上下文.4,确保不要遗漏字幕.5,翻译完成后校验原始字幕和翻译字幕的最后3条的序列号,时间轴和字幕意思是否相同.如果不同,需要重新翻译后再输出.6,不要在输出的结果中添加除字幕以外的内容.7,遵守以上所有规则,你将得到1000美元的奖励",
+                    "content": f"你是一个专业的电影字幕翻译人员, 擅长从各种语言翻译为中文. 用户会将带有时间轴信息的字幕发送给你, 你要先完整理解字幕内容, 然后保留字幕的序列号和时间轴信息, 将字幕翻译为{self.target_lang}. 要求: 1,保持输出内容格式和输入内容格式相同.2,保留字幕中的人名和专有名词和缩写.3,输出内容要符合中文使用习惯,也要符合上下文.4,确保不要遗漏字幕.5,翻译完成后校验原始字幕和翻译字幕的最后3条的序列号,时间轴和字幕意思是否相同.如果不同,请重新翻译.6,不要输出校验结果到字幕内, 否则你会被罚款100美元.7,遵守以上所有要求,完成后你将得到1000美元的奖励",
                 },
                 {
                     "role": "user",
@@ -132,13 +147,23 @@ class SubtitleGenerator(object):
             ],
         )
         translated_content = (
-            completion["choices"][0]  # type: ignore
-            .get("message")
-            .get("content")
-            .encode("utf-8")
-            .decode()
+            completion.choices[0].message.content.encode("utf-8").decode()  # type: ignore
         )
         return translated_content
+
+    def split_and_translate(self):
+        frag = math.ceil(len(self.source_subtitle) / self.max_split)
+        for index in range(0, len(self.source_subtitle), self.max_split):
+            print(f"\rHold my beer ... {(index // self.max_split) + 1}/{frag}", end="")
+            slices_subtitle = self.split_and_translate_chunk(index)
+            self.merge(slices_subtitle)
+        print("\rHold my beer ... Done!")
+
+    def split_and_translate_chunk(self, start: int) -> SSAFile:
+        slices_subtitle = self.split(self.source_subtitle, start)
+        translated_subtitle_string = self.translate(slices_subtitle)
+        translated_subtitle = SSAFile.from_string(translated_subtitle_string)
+        return translated_subtitle
 
     def generate_ass(self) -> None:
         print("生成ass字幕文件 ... ", end="")
@@ -170,7 +195,12 @@ class SubtitleGenerator(object):
             e.style = "bottom"
             subs.append(e)
 
-        subs.save(f"{self.video_name}.ass")
+        output_dir = os.path.dirname(self.video)
+        if self.output_dir:
+            output_dir = self.output_dir
+        else:
+            output_dir = self.input_dir
+        subs.save(os.path.join(output_dir, f"{self.video_name}.ass"))
         print("OK!")
 
 
@@ -195,25 +225,29 @@ def main():
         default=80,
         help="由于翻译模型的输出token限制,需要将字幕分隔后进行翻译. 建议不超过100. 过大会超出token限制, 过小会增加翻译时间.",
     )
+    parser.add_argument(
+        "-o, --output-dir",
+        type=str,
+        dest="outputdir",
+        default=None,
+        help="输出字幕文件所在目录",
+    )
 
     args = parser.parse_args()
 
-    subtitle_generator = SubtitleGenerator(args.video, max_split=args.maxsplit)
+    subtitle_generator = SubtitleGenerator(
+        args.video,
+        target_lang=args.targetlang,
+        max_split=args.maxsplit,
+        output_dir=args.outputdir,
+    )
 
     subtitle_file = subtitle_generator.extract()
     if not subtitle_file:
         print("没有字幕文件被导出")
         return
     subtitle_generator.load(subtitle_file=subtitle_file)
-    print("Hold my beer ... ")
-    for index in range(0, len(subtitle_generator.source_subtitle), args.maxsplit):
-        slices_subtitle = subtitle_generator.split(
-            subtitle_generator.source_subtitle, start=index
-        )
-        translated_subtitle_string = subtitle_generator.translate(slices_subtitle)
-        translated_subtitle = SSAFile.from_string(translated_subtitle_string)
-        subtitle_generator.merge(translated_subtitle)
-    print("Done")
+    subtitle_generator.split_and_translate()
     subtitle_generator.generate_ass()
 
 
